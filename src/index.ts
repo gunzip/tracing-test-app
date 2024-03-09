58bb6bda0b5c97608eac30393d49719fc8e17058
danilo spinelli <danilo.spinelli@pagopa.it>
2024-03-09 18:36:54 +0100

    test changes

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import express, { Request, Response } from "express";
 
 const app = express();
 
-app.get("/", (req: Request, res: Response) => res.send("Hello World!"));
+app.get("/", (req: Request, res: Response) => res.send("Hello World!!"));
 
 const port: string | number = process.env.PORT || 3000;
 
