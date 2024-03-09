97c86b1116e0cbe06d3f9027637c033e8e27a2b4
danilo spinelli <danilo.spinelli@pagopa.it>
2024-03-09 18:32:12 +0100

    init

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import express, { Request, Response } from "express";
+
+const app = express();
+
+app.get("/", (req: Request, res: Response) => res.send("Hello World!"));
+
+const port: string | number = process.env.PORT || 3000;
+
+app.listen(port, () => console.log(`App listening on port ${port}`));
