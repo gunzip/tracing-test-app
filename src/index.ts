56f053184c8d628f273673584b0129c75ca50a15
danilo spinelli <danilo.spinelli@pagopa.it>
2024-03-11 23:18:04 +0100

    fix sources

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,30 @@
-import express, { Request, Response } from "express";
-
-// Import the `useAzureMonitor()` function from the `@azure/monitor-opentelemetry` package.
 import { useAzureMonitor } from "@azure/monitor-opentelemetry";
 
-import redis from "redis";
+// Call the `useAzureMonitor()` function to configure OpenTelemetry to use Azure Monitor.
+useAzureMonitor({
+  azureMonitorExporterOptions: {
+    connectionString:
+      process.env["APPLICATIONINSIGHTS_CONNECTION_STRING"] ||
+      "<your connection string>",
+  },
+  instrumentationOptions: {
+    // Instrumentations generating traces
+    azureSdk: { enabled: true },
+    http: { enabled: true },
+    mongoDb: { enabled: true },
+    mySql: { enabled: true },
+    postgreSql: { enabled: true },
+    redis: { enabled: true },
+    redis4: { enabled: true },
+  },
+  samplingRatio: 1.0,
+  enableLiveMetrics: true,
+  enableStandardMetrics: true,
+});
+
+import express, { Request, Response } from "express";
+
+import * as redis from "redis";
 
 // Environment variables for cache
 const cacheHostName = process.env.AZURE_CACHE_FOR_REDIS_HOST_NAME;
@@ -37,28 +58,6 @@ app.get("/", (req: Request, res: Response) => res.send("Hello World!!!"));
 const port: string | number = process.env.PORT || 3000;
 
 app.listen(port, () => console.log(`App listening on port ${port}`));
-
-// Call the `useAzureMonitor()` function to configure OpenTelemetry to use Azure Monitor.
-useAzureMonitor({
-  azureMonitorExporterOptions: {
-    connectionString:
-      process.env["APPLICATIONINSIGHTS_CONNECTION_STRING"] ||
-      "<your connection string>",
-  },
-  instrumentationOptions: {
-    // Instrumentations generating traces
-    azureSdk: { enabled: true },
-    http: { enabled: true },
-    mongoDb: { enabled: true },
-    mySql: { enabled: true },
-    postgreSql: { enabled: true },
-    redis: { enabled: true },
-    redis4: { enabled: true },
-  },
-  samplingRatio: 1.0,
-  enableLiveMetrics: true,
-  enableStandardMetrics: true,
-});
 
 // import { NodeTracerProvider } from "@opentelemetry/sdk-trace-node";
 // import { registerInstrumentations } from "@opentelemetry/instrumentation";
