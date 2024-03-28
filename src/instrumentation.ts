4040e6cc2bb82365d7f60324eb78b35b339ee5ac
AleDore <alessio85.dore@gmail.com>
2024-03-28 17:01:25 +0100

    Test Upgrade appinsights SDK

--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -1,26 +1,32 @@
-import { useAzureMonitor } from "@azure/monitor-opentelemetry";
-
+import { initAppInsights } from "@pagopa/ts-commons/lib/appinsights";
 // Call the `useAzureMonitor()` function to configure OpenTelemetry to use Azure Monitor.
-useAzureMonitor({
-  azureMonitorExporterOptions: {
-    connectionString:
-      process.env["APPLICATIONINSIGHTS_CONNECTION_STRING"] ||
-      "<your connection string>",
-  },
-  instrumentationOptions: {
-    // Instrumentations generating traces
-    azureSdk: { enabled: true },
-    http: { enabled: true },
-    mongoDb: { enabled: true },
-    mySql: { enabled: true },
-    postgreSql: { enabled: true },
-    redis: { enabled: false },
-    redis4: { enabled: true },
-  },
-  samplingRatio: 1.0,
-  enableLiveMetrics: true,
-  enableStandardMetrics: true,
-});
+
+initAppInsights(
+  process.env["APPLICATIONINSIGHTS_CONNECTION_STRING"] ||
+    "<your connection string>",
+  { cloudRole: "test-ai", samplingPercentage: 100 },
+  process.env,
+);
+// useAzureMonitor({
+//   azureMonitorExporterOptions: {
+//     connectionString:
+//      process.env["APPLICATIONINSIGHTS_CONNECTION_STRING"] ||
+//   "<your connection string>",
+//   },
+//   instrumentationOptions: {
+//     // Instrumentations generating traces
+//     azureSdk: { enabled: true },
+//     http: { enabled: true },
+//     mongoDb: { enabled: true },
+//     mySql: { enabled: true },
+//     postgreSql: { enabled: true },
+//     redis: { enabled: false },
+//     redis4: { enabled: true },
+//   },
+//   samplingRatio: 1.0,
+//   enableLiveMetrics: true,
+//   enableStandardMetrics: true,
+// });
 
 // import { registerInstrumentations } from "@opentelemetry/instrumentation";
 // import {
