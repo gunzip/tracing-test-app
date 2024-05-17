264590906a413f40df46864cb695e4284836a6fb
danilo spinelli <danilo.spinelli@pagopa.it>
2024-05-17 21:43:10 +0200

    fix

--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -44,10 +44,10 @@ registerInstrumentations({
 // does this work?
 // defaultClient.setAutoPopulateAzureProperties();
 
+setup(aiConnectionString).start();
+
 defaultClient.context.tags[defaultClient.context.keys.cloudRole] =
   "function-test-tracing";
-
-setup(aiConnectionString).start();
 
 // import { registerInstrumentations } from "@opentelemetry/instrumentation";
 // import {
