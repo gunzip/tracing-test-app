6e2d3961767f04b1af10e2bc2f68400f26f83300
danilo spinelli <danilo.spinelli@pagopa.it>
2024-05-17 16:00:55 +0200

    Add function

--- src/functions.ts
+++ src/functions.ts
@@ -1,167 +1,125 @@
-// import { CosmosClient } from "@azure/cosmos";
-// import * as redis from "redis";
+import { CosmosClient } from "@azure/cosmos";
+import * as redis from "redis";
+import { app } from "@azure/functions";
 
-// import type { Request, Response } from "express";
+// import { DiagConsoleLogger, DiagLogLevel, diag } from "@opentelemetry/api";
+// diag.setLogger(new DiagConsoleLogger(), DiagLogLevel.ALL);
 
-// // import { DiagConsoleLogger, DiagLogLevel, diag } from "@opentelemetry/api";
-// // diag.setLogger(new DiagConsoleLogger(), DiagLogLevel.ALL);
+// Environment variables for redis cache
+const cacheHostName = process.env.AZURE_CACHE_FOR_REDIS_HOST_NAME;
+const cachePassword = process.env.AZURE_CACHE_FOR_REDIS_ACCESS_KEY;
 
-// // Environment variables for redis cache
-// const cacheHostName = process.env.AZURE_CACHE_FOR_REDIS_HOST_NAME;
-// const cachePassword = process.env.AZURE_CACHE_FOR_REDIS_ACCESS_KEY;
+if (!cacheHostName) throw Error("AZURE_CACHE_FOR_REDIS_HOST_NAME is empty");
+if (!cachePassword) throw Error("AZURE_CACHE_FOR_REDIS_ACCESS_KEY is empty");
 
-// if (!cacheHostName) throw Error("AZURE_CACHE_FOR_REDIS_HOST_NAME is empty");
-// if (!cachePassword) throw Error("AZURE_CACHE_FOR_REDIS_ACCESS_KEY is empty");
+const cacheConnection = redis.createClient({
+  // rediss for TLS
+  url: `rediss://${cacheHostName}:6380`,
+  password: cachePassword,
+});
 
-// const cacheConnection = redis.createClient({
-//   // rediss for TLS
-//   url: `rediss://${cacheHostName}:6380`,
-//   password: cachePassword,
-// });
+// const app = express();
 
-// // const app = express();
+import * as otel from "@opentelemetry/api";
 
-// import * as otel from "@opentelemetry/api";
+app.get("/", async (req) => ({ body: `Hello, ${req.query.get("name")}!` }));
 
-// // async function fetchWithTracing(
-// //   url: string,
-// //   init?: RequestInit,
-// // ): Promise<globalThis.Response> {
-// //   // Ottiene il contesto attivo di OpenTelemetry
-// //   const currentContext = otel.context.active();
+// Query cosmos db
 
-// //   // Prepara gli header di tracciamento utilizzando il propagatore di OpenTelemetry
-// //   const headersWithTracing = {};
-// //   otel.propagation.inject(currentContext, headersWithTracing);
+const client = new CosmosClient(process.env.COSMOS_DB_CONNECTION_STRING ?? "");
 
-// //   // Aggiunge gli header di tracciamento agli header della richiesta originale
-// //   const initWithTracing = {
-// //     ...init,
-// //     headers: {
-// //       ...(init?.headers || {}),
-// //       ...headersWithTracing,
-// //     },
-// //   };
+async function queryCollection() {
+  const { database } = await client.databases.createIfNotExists({
+    id: "SampleDB",
+  });
+  const { container } = await database.containers.createIfNotExists({
+    id: "Persons",
+  });
 
-// //   return fetch(url, initWithTracing);
-// // }
+  const querySpec = {
+    query: "SELECT * FROM c WHERE c.firstname = @value",
+    parameters: [
+      {
+        name: "@value",
+        value: "Eva",
+      },
+    ],
+  };
 
-// /** this is an express middleware that takes the query parameter named 'delay'
-//  * from the query string and delays the response by that amount of time in milliseconds.
-//  */
-// // app.use((req, _, next) => {
-// //   const delay = req.query.delay;
-// //   if (delay && typeof delay === "string") {
-// //     setTimeout(next, parseInt(delay, 10));
-// //   } else {
-// //     next();
-// //   }
-// // });
+  const { resources: items } = await container.items
+    .query(querySpec)
+    .fetchAll();
 
-// app.get("/", (req: Request, res: Response) => res.send("Hello World!!!"));
+  return items;
+}
 
-// const port: string | number = process.env.PORT || 3000;
+app.get("/query", async () => {
+  const span = otel.trace.getActiveSpan();
+  if (span) {
+    span.addEvent("test-event", {
+      eventKey: "event-value",
+      spanId: span.spanContext().spanId,
+    });
+  }
+  let r: any;
+  try {
+    r = await queryCollection();
+  } catch (err) {
+    return { jsonBody: { err } };
+  }
+  return { jsonBody: { r } };
+});
 
-// app.listen(port, () => console.log(`App listening on port ${port}`));
+/** the following express endpoint returns a response with the status code given into query parameter */
+app.get("/status", async (req) => {
+  const status = req.query.get("status");
+  if (status && typeof status === "string") {
+    return { status: parseInt(status, 10), jsonBody: { status: status } };
+  } else {
+    return { status: 200, jsonBody: { status } };
+  }
+});
 
-// // Query cosmos db
+(async function () {
+  await cacheConnection.connect();
+})();
 
-// const client = new CosmosClient(process.env.COSMOS_DB_CONNECTION_STRING ?? "");
+/** the following endpoint connects to a redis instance and get some info about it */
+app.get("/redis", async () => {
+  try {
+    await cacheConnection.set(
+      "Message",
+      "Hello! The cache is working from Node.js!",
+    );
+    const msg = await cacheConnection.get("Message");
+    return { jsonBody: { msg } };
+  } catch (error) {
+    return { status: 400, jsonBody: { error } };
+  }
+});
 
-// async function queryCollection() {
-//   const { database } = await client.databases.createIfNotExists({
-//     id: "SampleDB",
-//   });
-//   const { container } = await database.containers.createIfNotExists({
-//     id: "Persons",
-//   });
+app.get("/redis-db", async () => {
+  try {
+    await cacheConnection.set(
+      "Message",
+      "Hello! The v2 cache is working from Node.js!",
+    );
+    const msg = await cacheConnection.get("Message");
 
-//   const querySpec = {
-//     query: "SELECT * FROM c WHERE c.firstname = @value",
-//     parameters: [
-//       {
-//         name: "@value",
-//         value: "Eva",
-//       },
-//     ],
-//   };
+    // we call the internal endpoint to query
+    const data = await fetch(
+      process.env.WEBSITE_HOSTNAME
+        ? `https://${process.env.WEBSITE_HOSTNAME}/query`
+        : "http://localhost:3000/query",
+    ).then((r) => r.json());
 
-//   const { resources: items } = await container.items
-//     .query(querySpec)
-//     .fetchAll();
+    return { status: 200, jsonBody: { msg, data } };
+  } catch (error) {
+    return { status: 400, jsonBody: { error } };
+  }
+});
 
-//   return items;
-// }
-
-// app.get("/query", (_: Request, res: Response) => {
-//   const span = otel.trace.getActiveSpan();
-//   if (span) {
-//     span.addEvent("test-event", {
-//       eventKey: "event-value",
-//       spanId: span.spanContext().spanId,
-//     });
-//     diag.info("test log");
-//   }
-
-//   queryCollection()
-//     .catch((err) => {
-//       res.json(JSON.stringify(err));
-//     })
-//     .then((r) => {
-//       res.json(JSON.stringify(r));
-//     });
-// });
-
-// /** the following express endpoint returns a response with the status code given into query parameter */
-// app.get("/status", (req: Request, res: Response) => {
-//   const status = req.query.status;
-//   if (status && typeof status === "string") {
-//     res.status(parseInt(status, 10)).send({ status: status });
-//   } else {
-//     res.status(200).send({ status: status });
-//   }
-// });
-
-// (async function () {
-//   await cacheConnection.connect();
-// })();
-
-// /** the following endpoint connects to a redis instance and get some info about it */
-// app.get("/redis", async (_: Request, res: Response) => {
-//   try {
-//     await cacheConnection.set(
-//       "Message",
-//       "Hello! The cache is working from Node.js!",
-//     );
-//     const msg = await cacheConnection.get("Message");
-//     res.send(msg);
-//   } catch (error) {
-//     res.status(400).send(error);
-//   }
-// });
-
-// app.get("/redis-db", async (_: Request, res: Response) => {
-//   try {
-//     await cacheConnection.set(
-//       "Message",
-//       "Hello! The v2 cache is working from Node.js!",
-//     );
-//     const msg = await cacheConnection.get("Message");
-
-//     // we call the internal endpoint to query
-//     const data = await fetch(
-//       process.env.WEBSITE_HOSTNAME
-//         ? `https://${process.env.WEBSITE_HOSTNAME}/query`
-//         : "http://localhost:3000/query",
-//     ).then((r) => r.json());
-
-//     res.send(msg + JSON.stringify(data));
-//   } catch (error) {
-//     res.status(400).send(error);
-//   }
-// });
-
-// process.on("SIGINT", async () => {
-//   await cacheConnection.disconnect();
-//   process.exit();
-// });
+process.on("SIGINT", async () => {
+  await cacheConnection.disconnect();
+  process.exit();
+});
