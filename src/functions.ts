912d78d8f52ef5ac69faa8eac529ab748daf0e20
danilo spinelli <danilo.spinelli@pagopa.it>
2024-05-18 11:02:11 +0200

    try ai

--- src/functions.ts
+++ src/functions.ts
@@ -1,4 +1,18 @@
-require("./instrumentation");
+// require("./instrumentation");
+
+let appInsights = require("applicationinsights");
+appInsights
+  .setup(process.env["APPLICATIONINSIGHTS_CONNECTION_STRING"])
+  .setAutoCollectRequests(true)
+  .setAutoCollectPerformance(true, true)
+  .setAutoCollectExceptions(true)
+  .setAutoCollectDependencies(true)
+  .setAutoCollectConsole(true, false)
+  .setAutoCollectPreAggregatedMetrics(true)
+  .setSendLiveMetrics(false)
+  .setInternalLogging(false, true)
+  .enableWebInstrumentation(false)
+  .start();
 
 import { CosmosClient } from "@azure/cosmos";
 import * as redis from "redis";
